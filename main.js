235a66fcdac55ecbf0c3dc58a6a709691a01410e
AzamatTash <atashinov98@mail.ru>
2021-12-11 15:39:36 +0500

    add file

---
+++ main.js
@@ -0,0 +1,98 @@
+let value = document.querySelector('.title');
+value.innerHTML = '0';
+
+let operandOne = null;
+let operandTwo = null;
+let operator = null;
+let selectedOperator = null;
+
+const button = {
+    number : document.querySelectorAll('.calculator_row_item'),
+    operation : document.querySelectorAll('.calculator_row_item-operation'),
+    equals : document.querySelector('.calculator_row_item-equals'),
+    clear : document.querySelector('.calculator_row_item-clear'),
+    backSpace : document.querySelector('.calculator_row_item-back_space')
+}
+
+button.backSpace.addEventListener('click', function() {
+    let lenghtValue = value.innerHTML.length;
+    if (lenghtValue == 1) {
+        value.innerHTML = '0'
+    } else {
+        value.innerHTML = value.innerHTML.slice(0, lenghtValue - 1)
+    }
+})
+
+button.clear.addEventListener('click', function() {
+    value.innerHTML = '0';
+    operandOne = null;
+    operandTwo = null;
+    operator = null;
+})
+
+ 
+for (let number of button.number) {
+    number.addEventListener('click', function() {
+        if (value.innerHTML != 0 && value.innerHTML != selectedOperator) {
+            value.innerHTML += number.innerHTML;
+        } else {
+            value.innerHTML = number.innerHTML;
+        }
+    })		
+}  
+
+for (let elemOperation of button.operation) {
+    elemOperation.addEventListener('click', function() {
+        operator = elemOperation.id;
+
+        switch (operator) {
+            case "div":
+            selectedOperator = "÷";
+            break;
+            
+            case "mult":
+            selectedOperator = "*";
+            break;
+            
+            case "sub":
+            selectedOperator = "-";
+            break;
+            
+            case "sum":
+            selectedOperator = "+";
+            break;
+        }
+
+        if (operandOne === null) {
+            operandOne = value.innerHTML;
+            value.innerHTML = selectedOperator;
+        } else if (operandTwo === null) {
+            operandTwo = value.innerHTML;
+        }
+    })
+}
+
+button.equals.addEventListener('click', function() {
+    operandTwo = value.innerHTML;
+    value.innerHTML = calc(operandOne, operandTwo, operator);
+})
+
+function calc(operandOne, operandTwo, operator) {
+    switch (operator) {
+      case "div":
+        if (operandTwo === "0") {
+          return "Error!";
+        } else {
+          return operandOne / operandTwo;
+        }
+  
+      case "mult":
+        return operandOne * operandTwo;
+  
+      case "sub":
+        return operandOne - operandTwo;
+  
+      case "sum":
+        return +operandOne + +operandTwo;
+    }
+}
